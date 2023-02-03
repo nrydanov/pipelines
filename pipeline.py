cf9fb7d1998748b8d46d0e5c5118cced6eb8ded1
Nikita Rydanov <wrongwayboyy@gmail.com>
2023-02-03 21:03:19 +0400

    Complete workflow without CLI

---
+++ pipeline.py
@@ -0,0 +1,10 @@
+class Pipeline:
+  
+  def __init__(self, name, version, tasks) -> None:
+    self.name = name
+    self.version = version
+    self.tasks = tasks    
+    
+  def run(self):
+    for task in self.tasks:
+      task.run()
